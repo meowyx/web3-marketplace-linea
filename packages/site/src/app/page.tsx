cdd23b256f975db79e8dd1c8edaeb51f2ea784dd
meowyx <qafka256@gmail.com>
2024-09-04 20:39:47 +0600

    starter

---
+++ packages/site/src/app/page.tsx
@@ -0,0 +1,56 @@
+
+"use client";
+
+import { ConnectButton } from "@consensys/connect-button";
+import { client } from "@/providers/WagmiProvider";
+import { Suspense, useEffect, useState } from "react";
+import { useAccount } from "wagmi";
+
+export default function Home() {
+  const { isConnected, address } = useAccount();
+  const [blockNumber, setBlockNumber] = useState<bigint | null>(null);
+
+  useEffect(() => {
+    client.getBlockNumber().then((block) => {
+      setBlockNumber(block);
+    });
+  }, []);
+
+  return (
+    <main className="relative flex flex-col items-center gap-20 min-h-screen mx-auto md:p-24">
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className=" flex justify-center pt-10 md:pt-0 z-10 max-w-5xl w-full lg:items-center lg:justify-between font-mono text-sm lg:flex">
+          <div className="absolute bottom-0 left-0 flex w-full items-end justify-center lg:static lg:h-auto lg:w-auto lg:bg-none">
+            <a
+              className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              By RAD Team
+            </a>
+          </div>
+          <ConnectButton />
+        </div>
+
+        <div className="flex flex-col mt-52 items-center">
+          <span className="text-3xl font-bold">Web3 Starter template</span>
+          {isConnected && (
+            <span className="text-sm font-mono font-medium max-w-md text-center text-gray-500">
+              Connected to: {address}
+            </span>
+          )}
+
+          <div className="text-sm font-mono font-medium max-w-md text-center text-gray-500">
+            {!blockNumber ? (
+              "Loading block number..."
+            ) : (
+              <div>Linea block number: {Number(blockNumber)}</div>
+            )}
+          </div>
+        </div>
+      </Suspense>
+    </main>
+  );
+}
+    
